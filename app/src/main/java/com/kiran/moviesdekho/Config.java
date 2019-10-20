d0fdf3e047b7f4b33399acc99e78e659c17d6de0
KIRAN T RATHOD <kirantrathod.kr@gmail.com>
2019-10-20 10:56:02 +0530

    Create Config.java

---
+++ app/src/main/java/com/kiran/moviesdekho/Config.java
@@ -0,0 +1,8 @@
+package com.kiran.moviesdekho;
+
+public class Config {
+    public static String PACKAGE_NAME = "Your package name";
+    public final static String IMAGE_URL_BASE_PATH = "http://image.tmdb.org/t/p/w342//";
+    public static final String API_BASE_URL = "http://api.themoviedb.org/3/";
+    public final static String API_KEY = "Your API Key;
+}
